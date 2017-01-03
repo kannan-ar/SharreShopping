0b817322d9dab9fbb4e7bc9289762394173774a8
Kannan Ramachandran <kannanpala@gmail.com>
2017-01-03 07:45:59 +0530

    Style modifications

--- app/views/flipkart/flipkart-offer.component.ts
+++ app/views/flipkart/flipkart-offer.component.ts
@@ -6,12 +6,17 @@ import {FlipkartOffer} from "../../models/flipkart/flipkart-offer";
         selector: 'flipkart-offer',
         template: `
         <div class="col-md-3 col-sm-4 col-xs-12">
-                <div class="text-center flipkart-deal-item">
+                <div class="text-center flipkart-offer-item">
                         <h4><a href="{{item.url}}">{{item.title}}</a></h4>
                          <img-holder [thumbnail]="imageUrl"></img-holder>
                         <p>{{item.description}}</p>
                 </div>
-        </div>`
+        </div>`,
+        styles: [`
+                .flipkart-offer-item {
+                        border: 1px solid #14819C;
+                }
+        `]
 })
 
 export class FlipkartOfferComponent {
